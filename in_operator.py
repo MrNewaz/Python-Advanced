6ffa8657860d2b8520881b63ffbc056b25903f14
MrNewaz <saifur.saif@northsouth.edu>
2021-07-05 19:25:48 +0600

    init

---
+++ in_operator.py
@@ -0,0 +1,18 @@
+my_list = ["Saif", "Newaz"]
+
+my_answer = input("What is your name? ")
+
+
+if my_answer in my_list:
+    print(my_answer)
+else:
+    print("Wrong Answer")
+
+dickto = {
+    "name": "Saif",
+    "job": "Shetai"
+}
+
+
+if "name" in dickto:
+    print("Hmm")
