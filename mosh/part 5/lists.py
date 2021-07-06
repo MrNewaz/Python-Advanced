c70a650ec8aaf8d70fc54a0c53ffa652019a5d13
MrNewaz <saifur.saif@northsouth.edu>
2021-07-07 01:26:13 +0600

    init

---
+++ mosh/part 5/lists.py
@@ -0,0 +1,10 @@
+zeros = [0] * 100
+
+ones = [1] * 100
+combined = zeros+ones
+
+numbers = list(range(20))
+
+chars = list('Saif Newaz')
+
+print(len(chars))
---
+++ mosh/part 5/listsFunc.py
@@ -0,0 +1,6 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f']
+
+numbers = list(range(20))
+
+# every 2nd item
+print(numbers[::-1])
