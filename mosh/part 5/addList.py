e00dd159c3af9ebda40ff7ce2893d653fbdcc627
MrNewaz <saifur.saif@northsouth.edu>
2021-07-07 02:23:29 +0600

    5done

---
+++ mosh/part 5/addList.py
@@ -0,0 +1,13 @@
+letters = ['a', 'b', 'c']
+
+letters.insert(0, '-')
+print(letters)
+
+letters.remove('b')
+print(letters)
+
+del letters[0:2]
+print(letters)
+
+letters.clear()
+print(letters)
---
+++ mosh/part 5/looping.py
@@ -0,0 +1,4 @@
+letters = ['a', 'b', 'c']
+
+for i, value in enumerate(letters):
+    print(i, value)
